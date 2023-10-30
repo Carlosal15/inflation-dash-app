2b51bc77f9b275d96e19845e5c0ce69ef150fa72
Carlos <carlosal1993@gmail.com>
2023-10-30 04:36:55 +0000

    fixed location of the logger to source code folder

--- inflation_app/logger_config.py
+++ inflation_app/logger_config.py
@@ -1,10 +1,13 @@
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
 logger.setLevel(logging.WARNING)
 
-handler = logging.FileHandler("inflation_app.log")
+log_dir = os.path.dirname(os.path.abspath(__file__))
+log_file = os.path.join(log_dir, "inflation_app.log")
+handler = logging.FileHandler(log_file)
 
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
